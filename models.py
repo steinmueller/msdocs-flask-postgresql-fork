895fe0e94dc152ad95e21ef444db2ee8a209c8b6
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-23 16:25:29 +0200

    update

--- models.py
+++ models.py
@@ -1,32 +1,18 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.orm import validates
-
 from app import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
 
+class Mission(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    data_ecg = db.Column(db.String)
+    # add more datatypes if necessary
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    local_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #remote_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    street_address = Column(String(50))
-    description = Column(String(250))
-
-    def __str__(self):
-        return self.name
-
-class Review(db.Model):
-    __tablename__ = 'review'
-    id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
-    user_name = Column(String(30))
-    rating = Column(Integer)
-    review_text = Column(String(500))
-    review_date = Column(DateTime)
-
-    @validates('rating')
-    def validate_rating(self, key, value):
-        assert value is None or (1 <= value <= 5)
-        return value
-
-    def __str__(self):
-        return f"{self.user_name}: {self.review_date:%x}"
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    missions = db.relationship('Mission')
