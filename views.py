9e03a41df029931fe48fd5988a527b7a29205e48
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-23 14:07:20 +0200

    update modules

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from flask import Blueprint, render_template
 from . import get_app
 
 app = get_app()
