5eaa4745f5002d4505d42843fdaf6e536a938588
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-23 13:59:56 +0200

    integrate app

---
+++ views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from flask import Blueprint, render_template
+from . import get_app
+
+app = get_app()
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET', 'POST'])
+def landingpage():
+    return render_template("landingpage.html", user=current_user)
+
+# TODO add functionalities for the web app
+@views.route('/home')
+@login_required
+#@oidc.require_login
+def home():
+    return render_template("home.html", user=current_user)
+
+@views.route('/app')
+@login_required
+def app():
+    return render_template("app.html", user=current_user)
